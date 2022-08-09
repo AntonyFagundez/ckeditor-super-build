b4e5332cb4c0f0d77868c39fd04c766bc7a438cd
AntonyF-Andreani <afagundez@andreani.com>
2022-08-09 19:36:25 -0300

    feat: fix version

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -2,41 +2,41 @@
  * @license Copyright (c) 2014-2022, CKSource Holding sp. z o.o. All rights reserved.
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
-import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
-import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
-import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
-import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
-import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
-import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
-import Image from '@ckeditor/ckeditor5-image/src/image.js';
-import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption.js';
-import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle.js';
-import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar.js';
-import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload.js';
-import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
-import IndentBlock from '@ckeditor/ckeditor5-indent/src/indentblock.js';
-import IndentBlockCommand from '@ckeditor/ckeditor5-indent/src/indentblockcommand.js';
-import IndentEditting from '@ckeditor/ckeditor5-indent/src/indentediting.js';
-import IndentUi from '@ckeditor/ckeditor5-indent/src/indentui.js';
-import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
-import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough.js';
-import Subscript from '@ckeditor/ckeditor5-basic-styles/src/subscript.js';
-import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline.js';
-import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript.js';
-import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment.js';
-import Font from '@ckeditor/ckeditor5-font/src/font.js';
-import FontBackgroundColor from '@ckeditor/ckeditor5-font/src/fontbackgroundcolor.js';
-import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor.js';
-import FontSize from '@ckeditor/ckeditor5-font/src/fontsize.js';
-import Table from '@ckeditor/ckeditor5-table/src/table.js'
-import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar.js'
-import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed.js';
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
+import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading';
+import Image from '@ckeditor/ckeditor5-image/src/image';
+import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
+import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
+import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
+import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
+import Indent from '@ckeditor/ckeditor5-indent/src/indent';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
+import Subscript from '@ckeditor/ckeditor5-basic-styles/src/subscript';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
+import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
+import Font from '@ckeditor/ckeditor5-font/src/font';
+import FontBackgroundColor from '@ckeditor/ckeditor5-font/src/fontbackgroundcolor';
+import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor';
+import FontSize from '@ckeditor/ckeditor5-font/src/fontsize';
+import Table from '@ckeditor/ckeditor5-table/src/table'
+import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar'
+import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
 
-import Link from '@ckeditor/ckeditor5-link/src/link.js';
-import List from '@ckeditor/ckeditor5-list/src/list.js';
-import Mention from '@ckeditor/ckeditor5-mention/src/mention.js';
-import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
-import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation.js';
+import Link from '@ckeditor/ckeditor5-link/src/link';
+import List from '@ckeditor/ckeditor5-list/src/list';
+import Mention from '@ckeditor/ckeditor5-mention/src/mention';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
+
+
+
+
 
 class Editor extends ClassicEditor {}
 
@@ -71,10 +71,6 @@ Editor.builtinPlugins = [
 	FontBackgroundColor,
 	FontColor,
 	FontSize,
-	IndentBlock , 
-	IndentBlockCommand,
- 	IndentEditting,
- 	IndentUi
 ];
 
 // Editor configuration.

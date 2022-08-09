c02145beda284ebf62e8975c66e2579e2ecf74db
AntonyF-Andreani <afagundez@andreani.com>
2022-08-09 19:09:29 -0300

    feat: plugins stables

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -6,7 +6,6 @@ import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
-import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices.js';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
 import Image from '@ckeditor/ckeditor5-image/src/image.js';
@@ -15,15 +14,28 @@ import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle.js';
 import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar.js';
 import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload.js';
 import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
+import IndentBlock from '@ckeditor/ckeditor5-indent/src/indentblock.js';
+import IndentBlockCommand from '@ckeditor/ckeditor5-indent/src/indentblockcommand.js';
+import IndentEditting from '@ckeditor/ckeditor5-indent/src/indentediting.js';
+import IndentUi from '@ckeditor/ckeditor5-indent/src/indentui.js';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough.js';
+import Subscript from '@ckeditor/ckeditor5-basic-styles/src/subscript.js';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline.js';
+import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript.js';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment.js';
+import Font from '@ckeditor/ckeditor5-font/src/font.js';
+import FontBackgroundColor from '@ckeditor/ckeditor5-font/src/fontbackgroundcolor.js';
+import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor.js';
+import FontSize from '@ckeditor/ckeditor5-font/src/fontsize.js';
+import Table from '@ckeditor/ckeditor5-table/src/table.js'
+import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar.js'
+import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed.js';
+
 import Link from '@ckeditor/ckeditor5-link/src/link.js';
 import List from '@ckeditor/ckeditor5-list/src/list.js';
-import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed.js';
 import Mention from '@ckeditor/ckeditor5-mention/src/mention.js';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
-import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice.js';
-import Table from '@ckeditor/ckeditor5-table/src/table.js';
-import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar.js';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation.js';
 
 class Editor extends ClassicEditor {}
@@ -33,7 +45,6 @@ Editor.builtinPlugins = [
 	Autoformat,
 	BlockQuote,
 	Bold,
-	CloudServices,
 	Essentials,
 	Heading,
 	Image,
@@ -47,11 +58,23 @@ Editor.builtinPlugins = [
 	List,
 	MediaEmbed,
 	Mention,
-	Paragraph,
-	PasteFromOffice,
 	Table,
 	TableToolbar,
-	TextTransformation
+	Paragraph,
+	TextTransformation,
+	Strikethrough,
+	Subscript,
+	Underline,
+	Superscript,
+	Alignment,
+	Font,
+	FontBackgroundColor,
+	FontColor,
+	FontSize,
+	IndentBlock , 
+	IndentBlockCommand,
+ 	IndentEditting,
+ 	IndentUi
 ];
 
 // Editor configuration.
